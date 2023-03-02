df73991f2a87c676fb4e0b871ad45afacd90d45f
ThomasMumladze <mumladzea4@gmail.com>
2023-03-02 10:45:49 +0400

    factorial

--- factorial.py
+++ factorial.py
@@ -5,3 +5,32 @@ def fact (n):
 
 result = fact(5)
 print(result)
+
+
+
+'''
+
+def fact (n):
+    if 5 == 1:
+        return 1
+    return fact(24) * 5  -->  20 play -->  120
+    
+    if n=4 == 1:
+        return 1
+    return fact(6) * 4   --> 12  play --> 24 
+    
+    if n=3 == 1
+        return 1
+    return fact(2) * 3   --> 6 play --> 6
+    
+    if n=2 == 1
+        return 1
+    return fact(1) * 2   -->  2 play --> 2
+    
+    if n=1 == 1
+        return 1
+    
+fact(5)
+
+'''
+
